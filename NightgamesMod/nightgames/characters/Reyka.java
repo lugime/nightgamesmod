c62091a2317e3a3e7ae66d8b8c3d116fce8fbf6b
DarkSinfulMage <darksinfulmage@gmail.com>
2017-05-10 23:08:21 -0400

    Test Commit: Reyka Dark Goddess Description.

--- NightgamesMod/nightgames/characters/Reyka.java
+++ NightgamesMod/nightgames/characters/Reyka.java
@@ -380,16 +380,26 @@ public class Reyka extends BasePersonality {
         });
 
         character.addLine(CharacterLine.DESCRIBE_LINER, (c, self, other) -> {
-            return "Reyka the succubus stands before you, six feet tall with"
-                            + " the most stunningly beautiful body you have ever seen."
-                            + " Her long black hair enshrines her perfect face like a priceless"
-                            + " painting. Her arms are slim and end in long-fingered,"
-                            + " soft hands, nails polished shining red. Underneath, her long and"
-                            + " perfectly formed legs and delicate feet stand in an imposing posture."
-                            + " Behind her, you see a pair of relatively small but powerful-looking bat wings.<br/>"
-                            + " Her gaze speaks of indescribable pleasure, but your mind reminds you"
-                            + " of the cost of indulging in a succubus' body: Give her half a chance"
-                            + " and she will suck out your very soul.";
+            
+            if (self.get(Attribute.Divinity) > 15) { 
+               return "Reyka is no longer just a regular succubus – as if her being a succubus wasn't already scary enough: Standing before you is a dark goddess radiating a terrifying aura of unholy power. "
+                                + "Reyka remains tall in stature, but her alluring face and beautiful body now call to you to prostrate yourself before her and submit to her will. Her hands with their "
+                                + "red-polished nails now look as horribly cruel as they do enticing, gentle, and soft. Underneath, her long and perfectly formed legs and delicate feet stand in an "
+                                + "imposing posture. Behind her, you see a pair of magnificent looking bat-like wings, commanding you to submit to her embrace and worship her.\n\n"
+                                + "Her gaze both captivates you and terrifies you - it's a gaze of of indescribable pleasure and unknown power. She'll do worse than suck out your soul...far worse.";
+            } else {
+                 return "Reyka the succubus stands before you, six feet tall with"
+                                + " the most stunningly beautiful body you have ever seen."
+                                + " Her long black hair enshrines her perfect face like a priceless"
+                                + " painting. Her arms are slim and end in long-fingered,"
+                                + " soft hands, nails polished shining red. Underneath, her long and"
+                                + " perfectly formed legs and delicate feet stand in an imposing posture."
+                                + " Behind her, you see a pair of relatively small but powerful-looking bat wings.<br/>"
+                                + " Her gaze speaks of indescribable pleasure, but your mind reminds you"
+                                + " of the cost of indulging in a succubus' body: Give her half a chance"
+                                + " and she will suck out your very soul.";
+            }
+
         });
 
         character.addLine(CharacterLine.LEVEL_DRAIN_LINER, (c, self, other) -> {
